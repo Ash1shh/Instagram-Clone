3653f50bc6df48ab1c9976265e663820807b607e
Ash1shh <ashishchandra8328@gmail.com>
2023-06-07 21:54:10 +0530

    :heart: Instagram-Clone

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   swcMinify: true,
   images: {
     domains: ["lh3.googleusercontent.com","cloudflare-ipfs.com"],
   }
+  
 };
 
 module.exports = nextConfig;
